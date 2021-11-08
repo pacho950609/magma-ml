0d7c57b1d3ae32a9737318050770964a12a38f2d
Francisco Ricaurte <ricaurte95fj@gmail.com>
2021-11-08 10:06:20 -0500

    crear funcion para subir y bajar ancho de banda

--- ver.py
+++ ver.py
@@ -5,6 +5,7 @@ from sklearn.datasets import load_iris
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
 import requests
+import json
 
 # create a pipeline object
 pipe = make_pipeline(
@@ -19,9 +20,55 @@ X, y = load_iris(return_X_y=True)
 print(len(X))
 print(len(y),y)
 
-response = requests.get('http://localhost:9090/api/v1/query?query=up')
-print('aca')
-print(response.json()['data']['result'][0]['value'])
-print('aca2')
-response2 = requests.get('https://localhost:9443/magma/v1/lte/uniandes_network_01/subscribers/IMSI901700100001113',cert=('../../admin_operator.pem','../../admin_operator.pem'),verify=False)
-print(response2.json())
+# response = requests.get('http://localhost:9090/api/v1/query?query=up')
+# print('aca')
+# print(response.json()['data']['result'][0]['value'])
+# print('aca2')
+
+# print(response2.json())
+
+def upgradeBandwidth():
+    response2 = requests.get('https://localhost:9443/magma/v1/lte/uniandes_network_01/subscribers/IMSI901700100001113',cert=('../../admin_operator.pem','../../admin_operator.pem'),verify=False)
+    data = response2.json()
+    currentPolicy = data['active_policies'][0]
+    if currentPolicy == 'low_policy': 
+        newPolicy = ['medium_policy']
+        print('incrementado a medium')
+    elif currentPolicy == 'medium_policy':
+        newPolicy = ['high_policy']
+        print('incrementado a high')
+    else:
+        newPolicy = ['high_policy']
+        print('Esta en el maximo')
+
+    data['active_policies'] = newPolicy
+    requestBody = json.dumps(data)
+    headers={
+    'Content-type':'application/json', 
+    'Accept':'application/json'
+    }
+    responseUpgrade = requests.put('https://localhost:9443/magma/v1/lte/uniandes_network_01/subscribers/IMSI901700100001113',headers=headers,data=requestBody,cert=('../../admin_operator.pem','../../admin_operator.pem'),verify=False)
+
+def downgradeBandwidth():
+    response2 = requests.get('https://localhost:9443/magma/v1/lte/uniandes_network_01/subscribers/IMSI901700100001113',cert=('../../admin_operator.pem','../../admin_operator.pem'),verify=False)
+    data = response2.json()
+    currentPolicy = data['active_policies'][0]
+    if currentPolicy == 'low_policy': 
+        newPolicy = ['low_policy']
+        print('Esta en minimo')
+    elif currentPolicy == 'medium_policy':
+        newPolicy = ['low_policy']
+        print('disminuyendo a low')
+    else:
+        newPolicy = ['medium_policy']
+        print('disminuyendo a low')
+
+    data['active_policies'] = newPolicy
+    requestBody = json.dumps(data)
+    headers={
+    'Content-type':'application/json', 
+    'Accept':'application/json'
+    }
+    responseUpgrade = requests.put('https://localhost:9443/magma/v1/lte/uniandes_network_01/subscribers/IMSI901700100001113',headers=headers,data=requestBody,cert=('../../admin_operator.pem','../../admin_operator.pem'),verify=False)
+
+downgradeBandwidth()
