e036a4c02807f98f15c0e0a2cc679d4a68c96d91
Francisco Ricaurte <ricaurte95fj@gmail.com>
2021-11-14 19:05:25 -0500

    correr si la predicion da 1

--- dataAnalysis/dataAnalysis.py
+++ dataAnalysis/dataAnalysis.py
@@ -2,6 +2,6 @@ from integration.integration import upgradeBandwidthCall
 
 def processPrediction(prediction):
     print('Entro')
-    if(prediction == 0):
+    if(prediction == 1):
         print('Call upgrade bandwidth')
         upgradeBandwidthCall()
