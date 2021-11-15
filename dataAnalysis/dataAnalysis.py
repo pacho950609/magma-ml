2399ffe81f5eea97f9689d91dda6b007fc08293b
Francisco Ricaurte <ricaurte95fj@gmail.com>
2021-11-14 19:08:20 -0500

    agregar caso respuesta nula

--- dataAnalysis/dataAnalysis.py
+++ dataAnalysis/dataAnalysis.py
@@ -1,7 +1,6 @@
 from integration.integration import upgradeBandwidthCall
 
 def processPrediction(prediction):
-    print('Entro')
     if(prediction == 1):
         print('Call upgrade bandwidth')
         upgradeBandwidthCall()
--- dataProcessing/dataProcessing.py
+++ dataProcessing/dataProcessing.py
@@ -4,22 +4,29 @@ import time
 
 URL = 'http://localhost:9090'
 OutDataUsageQuery = 'sum(ue_reported_usage{IMSI="IMSI901700100001111",direction="up"})'
-OutThroughputQuery = 'avg(rate(ue_reported_usage{IMSI="IMSI901700100001111",direction="up"}[5m]))'
+OutThroughputQuery = 'avg(rate(ue_reported_usage{IMSI="IMSI901700100001111",direction="up"}[2m]))'
 
 
-def transformThroughput(throughputRange):
+def transformThroughputRange(throughputRange):
     return list(map(lambda throughput: float(throughput[1]), throughputRange))
+
+def transformThroughput(throughput):
+    return [float(throughput[1])]
 
 
 def getThroughputRange():
     end = time.time()
-    start = end - 180
-    path = URL + '/api/v1/query_range?query='+ OutThroughputQuery +'&start='+ str(start) + '&end='+ str(end) +'&step=80'
+    start = end - 240
+    # path = URL + '/api/v1/query_range?query='+ OutThroughputQuery +'&start='+ str(start) + '&end='+ str(end) +'&step=40'
+    path = URL + '/api/v1/query?query='+ OutThroughputQuery
     print('Throughput path: ' + path)
     response = requests.get(path)
-    throughputRange = response.json()['data']['result'][0]['values']
-    print('Throughput response:')
-    print(throughputRange)
+
+    if len(response.json()['data']['result']) == 0:
+        print('saltoooooooooooooo')
+        return []
+
+    throughputRange = response.json()['data']['result'][0]['value']
     transformedThroughput = transformThroughput(throughputRange)
     print('Transformed throughput:')
     print(transformedThroughput)
